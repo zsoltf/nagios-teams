d1ee96d6ca972e409d45e0ae0586ea751da42231
Zsolt Fekete <zsoltf@me.com>
2025-12-05 15:03:02 -0800

    add jinja template

--- nagios-teams.py
+++ nagios-teams.py
@@ -4,40 +4,113 @@ import requests
 import json
 import sys
 import os
+import jinja2
+
+template_string = """ {
+"type": "message",
+"attachments": [ {
+    "contentType":"application/vnd.microsoft.card.adaptive",
+    "contentUrl":null,
+    "content":{
+        "type": "AdaptiveCard",
+        "speak": "Host {{ name }} is DOWN",
+        "$schema": "https://adaptivecards.io/schemas/adaptive-card.json",
+        "version": "1.5",
+        "body": [
+            {
+                "type": "ColumnSet",
+                "columns": [
+                    {
+                        "type": "Column",
+                        "width": "auto",
+                        "items": [
+                            {
+                                "type": "Image",
+                                "url": "https://avatars.githubusercontent.com/u/5666660?s=200&v=4",
+                                "width": "42px",
+                                "height": "42px",
+                                "altText": "Logo",
+                                "targetWidth": "AtLeast:Standard",
+                                "style": "RoundedCorners"
+                                }
+                            ],
+                        "verticalContentAlignment": "Center"
+                        },
+                    {
+                        "type": "Column",
+                        "width": "stretch",
+                        "items": [
+                            {
+                                "type": "TextBlock",
+                                "text": "Host derp is DOWN",
+                                "wrap": true,
+                                "weight": "Bolder"
+                                },
+                            {
+                                "type": "TextBlock",
+                                "text": "Host derp",
+                                "wrap": true
+                                }
+                            ],
+                        "rtl": false
+                        },
+                    {
+                        "type": "Column",
+                        "width": "auto",
+                        "items": [
+                            {
+                                "type": "Icon",
+                                "name": "Warning",
+                                "color": "Attention",
+                                "style": "Filled",
+                                "size": "Medium",
+                                "horizontalAlignment": "Center"
+                                }
+                            ]
+                        }
+                    ],
+    "style": "attention"
+},
+{
+        "type": "ActionSet",
+        "actions": [
+            {
+                "type": "Action.OpenUrl",
+                "url": "https://adaptivecards.io/",
+                "iconUrl": "icon:CheckmarkCircle",
+                "style": "destructive"
+                },
+            {
+                "type": "Action.Submit",
+                "iconUrl": "icon:BookGlobe"
+                },
+            {
+                "type": "Action.OpenUrl",
+                "iconUrl": "icon:Eye"
+                }
+            ],
+        "horizontalAlignment": "Left",
+        "targetWidth": "AtLeast:Narrow"
+        }
+    ]
+}
+}]}"""
+
 
 def send_teams_notification(webhook_url, title, message):
     headers = {
-        "Content-Type": "application/json"
-    }
-    payload = {
-        "type": "message",
-        "attachments": [
-            {
-                "contentType": "application/vnd.microsoft.card.adaptive",
-                "content": {
-                    "$schema": "http://adaptivecards.io/schemas/adaptive-card.json",
-                    "type": "AdaptiveCard",
-                    "version": "1.0",
-                    "body": [
-                        {
-                            "type": "TextBlock",
-                            "text": title,
-                            "weight": "bolder",
-                            "size": "medium"
-                        },
-                        {
-                            "type": "TextBlock",
-                            "text": message,
-                            "wrap": True
-                        }
-                    ]
-                }
+            "Content-Type": "application/json"
             }
-        ]
-    }
+    payload = {
+            "type": "message",
+            "attachments": [
+                render_template("host")
+                ]
+            }
 
     # In Python 2, json.dumps() returns a str, which is what requests.post() expects for the 'data' argument.
-    response = requests.post(webhook_url, headers=headers, data=json.dumps(payload))
+    #response = requests.post(webhook_url, headers=headers, data=json.dumps(payload))
+    response = requests.post(webhook_url, headers=headers, data=render_template(notificationtype="host"))
 
     if response.status_code != 202:
         # Python 2 print statement and using old-style string formatting
@@ -45,6 +118,23 @@ def send_teams_notification(webhook_url, title, message):
     else:
         # Python 2 print statement
         print "Notification sent successfully"
+
+
+def render_template(notificationtype):
+
+    # Create a Jinja2 environment
+    env = jinja2.Environment()
+
+    # Load a template from a string
+    template = env.from_string(template_string)
+
+    if notificationtype == "service":
+        rendered_output = template.render(name="SERVICE")
+    else:
+        rendered_output = template.render(name="HOST")
+
+    return rendered_output
+
 
 if __name__ == "__main__":
     if len(sys.argv) != 4:
